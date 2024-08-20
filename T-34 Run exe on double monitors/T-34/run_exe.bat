72ba48bd54f349608af90f6ae29e7aec0a9797dd
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-08-20 12:20:14 +0300

    готово

--- T-34 Run exe on double monitors/T-34/run_exe.bat
+++ T-34 Run exe on double monitors/T-34/run_exe.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set "APP_PATH=%~dp0Configurator\Car_Configurator.exe"
 set "SHORTCUT_NAME=Car_Configurator.lnk"
 set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "NIRCMD_PATH=%~dp0T-34\nircmd-x64\nircmd.exe"
+set "NIRCMD_PATH=%~dp0nircmd-x64\nircmd.exe"
 
 if not exist "%STARTUP_FOLDER%\%SHORTCUT_NAME%" (
     echo Creating shortcut in Startup folder...
@@ -19,21 +19,26 @@ if not exist "%STARTUP_FOLDER%\%SHORTCUT_NAME%" (
     echo Shortcut already exists in Startup folder.
 )
 
-
-
 timeout /t 11 /nobreak
 
-@REM echo Stopping the explorer process
-@REM taskkill /F /IM explorer.exe
+REM Check if nircmd.exe exists
+if not exist "%NIRCMD_PATH%" (
+    echo Error: nircmd.exe not found at %NIRCMD_PATH%
+    exit /b 1
+)
 
-@REM if !ERRORLEVEL! NEQ 0 (
-@REM     echo Error stopping explorer.
-@REM     echo Restarting explorer...
-@REM     start explorer.exe
-@REM     echo Done!
-@REM     pause
-@REM     exit /b !ERRORLEVEL!
-@REM )
+echo Stopping the explorer process
+taskkill /F /IM explorer.exe
+
+if !ERRORLEVEL! NEQ 0 (
+    echo Error stopping explorer.
+    echo Restarting explorer...
+    start explorer.exe
+    echo Done!
+    pause
+    exit /b !ERRORLEVEL!
+)
+
 
 set "file=input.txt"
 
@@ -60,7 +65,7 @@ if "%fileValue%"=="1" (
     timeout /T 5 /NOBREAK >NUL
 
     REM Launch the first instance of the application on the first screen
-    echo Launching the first instance of %APP_EXE%...
+    echo Launching the first instance of %APP_PATH%...
     start "" "%APP_PATH%"
 
     REM Wait for the first instance to launch
@@ -75,7 +80,7 @@ if "%fileValue%"=="1" (
     )
 
     REM Launch the second instance of the application on the second screen
-    echo Launching the second instance of %APP_EXE%...
+    echo Launching the second instance of %APP_PATH%...
     start "" "%APP_PATH%"
 
 ) else if "%fileValue%"=="0" (
@@ -88,7 +93,7 @@ if "%fileValue%"=="1" (
     )
 
     REM Launch the second instance of the application on the second screen
-    echo Launching the second instance of %APP_EXE%...
+    echo Launching the second instance of %APP_PATH%...
     start "" "%APP_PATH%"
 
     REM Wait for the first instance to launch
@@ -101,28 +106,26 @@ if "%fileValue%"=="1" (
     timeout /T 5 /NOBREAK >NUL
 
     REM Launch the first instance of the application on the first screen
-    echo Launching the first instance of %APP_EXE%...
+    echo Launching the first instance of %APP_PATH%...
     start "" "%APP_PATH%"
 ) else (
     echo Error reading screen configuration, what value is in input.txt?
 )
 
-echo Press Space or Enter to reload explorer.exe %APP_EXE%...
+echo Press Space or Enter to reload explorer.exe %APP_PATH%...
 
 REM Wait for applications to finish
 :wait_for_exit
-tasklist /FI "IMAGENAME eq %APP_EXE%" 2>NUL | find /I /N "%APP_EXE%" >NUL
+tasklist /FI "IMAGENAME eq Car_Configurator.exe" 2>NUL | find /I /N "Car_Configurator.exe" >NUL
 if "!ERRORLEVEL!"=="0" (
     timeout /T 5 /NOBREAK >NUL
     goto wait_for_exit
 )
 
-echo All instances of %APP_EXE% are finished.
+echo All instances of Car_Configurator.exe are finished.
 echo Restarting explorer...
 start explorer.exe
 
-
 echo Done!
 timeout /t 2 /nobreak
-@REM exit /b
-taskkill /f /im cmd.exe >nul 2>&1
+exit /b
